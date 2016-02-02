d35b27c38ea514dbb354a16c1f75699e183f4063
Richard Rodriguez <richard.rodriguez@comparethemarket.com>
2016-02-02 20:25:54 +0000

    Next test, to drive mapping characters

--- TsarWarsTesting/integration_tests.cs
+++ TsarWarsTesting/integration_tests.cs
@@ -16,20 +16,7 @@ namespace TsarWarsTesting
             var filmDataProvider = new StubFilmDataProvider();
             const int episodeId = 4;
 
-            filmDataProvider.AddMovie(new Movie()
-            {
-                Title = "A New Hope",
-                EpisodeId = episodeId,
-                OpeningCrawl = "blah blah",
-                Director = "George Lucas",
-                Producers = new[] {"Gary Kurtz", "Rick McCallum"},
-                ReleaseDate = new DateTime(1977, 5, 25),
-                Characters = new List<Character>
-                {
-                    new Character {Name = "Luke Skywalker"},
-                    new Character {Name = "C-3PO"}
-                }
-            });
+            filmDataProvider.AddMovie(CreateMovie(episodeId));
 
             var tsarWars = new TsarWarsDataProcessor(filmDataProvider);
 
@@ -39,19 +26,85 @@ namespace TsarWarsTesting
             Assert.That(movieCharacters[0], Is.EqualTo("Aleksandr Orlov"));
             Assert.That(movieCharacters[1], Is.EqualTo("Bogdhan"));
         }
-    }
 
-    public class StubFilmDataProvider : IProvideFilmData
-    {
-        private readonly List<Movie> _movies = new List<Movie>();
-        public void AddMovie(Movie movie)
+        [Test]
+        public void different_characters_for_each_film_mapped_correctly()
         {
-            _movies.Add(movie);
+            var filmDataProvider = new StubFilmDataProvider();
+            const int episodeId = 1;
+
+            filmDataProvider.AddMovie(CreateMovie(episodeId));
+
+            var tsarWars = new TsarWarsDataProcessor(filmDataProvider);
+
+            var movieCharacters = tsarWars.FetchCharactersFor(episodeId);
+
+            Assert.That(movieCharacters.Count, Is.EqualTo(3));
+            Assert.That(movieCharacters[0], Is.EqualTo("Sergei"));
+            Assert.That(movieCharacters[1], Is.EqualTo("Maiya"));
+            Assert.That(movieCharacters[2], Is.EqualTo("Vassily"));
         }
 
-        public Movie GetMovieData(int episodeId)
+        private static Movie CreateMovie(int episodeId)
         {
-            return _movies.Single(m => m.EpisodeId == episodeId);
+            Movie movie;
+
+            switch (episodeId)
+            {
+                case 1:
+                    movie = new Movie
+                    {
+                        Title = "Phantom Menace",
+                        EpisodeId = episodeId,
+                        OpeningCrawl = "blah blah",
+                        Director = "George Lucas",
+                        Producers = new[] {"Gary Kurtz", "Rick McCallum"},
+                        ReleaseDate = new DateTime(1977, 5, 25),
+                        Characters = new List<Character>
+                        {
+                            new Character {Name = "Hans Solo"},
+                            new Character {Name = "Obi Wan Kenobi"},
+                            new Character {Name = "Darth Vader"}
+                        }
+                    };
+                    break;
+                case 4:
+                    movie = new Movie
+                    {
+                        Title = "A New Hope",
+                        EpisodeId = episodeId,
+                        OpeningCrawl = "blah blah",
+                        Director = "George Lucas",
+                        Producers = new[] {"Gary Kurtz", "Rick McCallum"},
+                        ReleaseDate = new DateTime(1977, 5, 25),
+                        Characters = new List<Character>
+                        {
+                            new Character {Name = "Luke Skywalker"},
+                            new Character {Name = "C-3PO"}
+                        }
+                    };
+                    break;
+                default:
+                    movie = null;
+                    break;
+            }
+
+            return movie;
+        }
+
+        public class StubFilmDataProvider : IProvideFilmData
+        {
+            private readonly List<Movie> _movies = new List<Movie>();
+
+            public void AddMovie(Movie movie)
+            {
+                _movies.Add(movie);
+            }
+
+            public Movie GetMovieData(int episodeId)
+            {
+                return _movies.Single(m => m.EpisodeId == episodeId);
+            }
         }
     }
 }
